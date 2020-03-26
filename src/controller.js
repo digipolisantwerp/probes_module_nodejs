c98ddec0dd8b89d5f990bfd702ccfb6a0d1e189a
Jan-Bart <janbart.vervoort@gmail.com>
2020-03-26 09:48:26 +0100

    Resolve PR remarks

--- src/controller.js
+++ src/controller.js
@@ -13,16 +13,12 @@ export default function createController(config = {}) {
       await Promise
         .all(
           alive.map(check => check())
-        )
-        .then(() => {
-          return res.json({ status: 200, message: 'ok' });
-        })
-        .catch(err => {
-          console.error(`PROBES_ERROR_1: ${err.message || ''}`);
-          return res.status(err.status || 500).json({ 'message': err.message || '' });
-        });
+        );
+
+      return res.json({ status: 200, message: 'ok' });
+
     } catch (error) {
-      console.error(`PROBES_ERROR_2: ${error.message || ''}`);
+      console.error(`PROBES_ERROR_1: ${error.message || ''}`);
       return res.status(error.status || 500).json({ message: error.message || '' });
     }
   }
@@ -32,16 +28,12 @@ export default function createController(config = {}) {
       await Promise
         .all(
           ready.map(check => check())
-        )
-        .then(() => {
-          return res.json({ status: 200, message: 'ok' });
-        })
-        .catch(err => {
-          console.error(`PROBES_ERROR_3: ${err.message || ''}`);
-          return res.status(err.status || 500).json({ 'message': err.message || '' });
-        });
+        );
+
+      return res.json({ status: 200, message: 'ok' });
+
     } catch (error) {
-      console.error(`PROBES_ERROR_4: ${error.message || ''}`);
+      console.error(`PROBES_ERROR_2: ${error.message || ''}`);
       return res.status(error.status || 500).json({ message: error.message || '' });
     }
   }
